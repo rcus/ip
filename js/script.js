2b5d0a9681be94626841068a27c3e10314081ebf
Marcus Törnroth <m@rcus.se>
2016-03-14 11:41:30 +0100

    Layout done, prepared for htaccess

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 $( document ).ready( function() {
-  $.get( "http://ipinfo.io", function( data ) {
+  var url = "http://ipinfo.io/";
+
+  $.get( url + window.location.pathname, function( data ) {
+    var location = (data.city ? data.city + ", " : "") + (data.region ? data.region + ", " : ""),
+        network = "<a href='" + url + data.org.substr(0, data.org.indexOf(" ")) + "'>" + data.org.substr(data.org.indexOf(" ")+1) + "</a>";
+
     $( document ).attr( "title", data.ip );
     $( "#header" ).text( data.ip );
-    $( ".location" ).text( data.city + ", " + data.region + ", " + data.country );
-    $( ".network" ).text( data.org );
+    $( ".location" ).text( location );
+    $( ".network" ).html( network );
     $( ".hostname" ).text( data.hostname );
+
+    $.get( "js/countries.json", function( c ) {
+      $( ".location" ).append( c[data.country] );
+    });
   }, "jsonp");
 
   $( ".toggleAbout" ).click( function() {
-    $( "#about" ).toggle();
+    $( "#about" ).fadeToggle();
   });
 });
