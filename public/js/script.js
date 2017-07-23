7f5ea9f3c505c89a4fcf6d915a58be9624fd2897
Marcus Törnroth <m@rcus.se>
2017-07-24 01:08:12 +0200

    Removed jQuery

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,43 @@
 (function(){
-  var url = "//ipinfo.io/";
+  var url = '//ipinfo.io/';
 
-  $.get( url, function( data ) {
-    var location = (data.city ? data.city + ", " : "") + (data.region ? data.region + ", " : ""),
-        network = "<a href='" + url + data.org.substr(0, data.org.indexOf(" ")) + "'>" + data.org.substr(data.org.indexOf(" ")+1) + "</a>";
+  function jsonp(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
 
-    $( document ).attr( "title", data.ip );
-    $( ".ip" ).text( data.ip );
-    $( ".location" ).text( location );
-    $( ".network" ).html( network );
-    $( ".hostname" ).text( data.hostname );
+    var script = document.createElement('script');
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+  }
 
-    $.get( "countries.json", function( c ) {
-      $( ".location" ).append( c[data.country] );
-    });
-  }, "jsonp");
+  jsonp(url, function(data) {
+    var location = (data.city ? data.city + ', ' : '') + (data.region ? data.region : ''),
+        network = '<a href="' + url + data.org.substr(0, data.org.indexOf(' ')) + '">' + data.org.substr(data.org.indexOf(' ') + 1) + '</a>';
+
+    document.title = data.ip;
+    document.querySelector('.ip').innerText = data.ip;
+    document.querySelector('.location').innerText = location;
+    document.querySelector('.network').innerHTML = network;
+    document.querySelector('.hostname').innerText = data.hostname || '';
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'countries.json', true);
+
+    request.onload = function() {
+      var country = ', ' + data.country;
+      if (request.status >= 200 && request.status < 400) {
+        var c = JSON.parse(request.responseText);
+        country = ', ' + c[data.country];
+      }
+      document.querySelector('.location').innerText += country;
+    };
+
+    request.send();
+  });
 
   document.querySelector('.about .header').innerText = window.location.hostname;
 
