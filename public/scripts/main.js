a114a87a19987cd4b5acb8e96a33220b7864590b
Marcus Törnroth <m@rcus.se>
2021-09-27 23:25:56 +0200

    Copy to clipboard on click

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,22 @@
 (function () {
   const url = 'https://json.geoiplookup.io/'
   const el = tag => document.getElementsByTagName(tag)[0]
-  const div = inner => {
+  const div = (inner, { copyOnClick } = {}) => {
     const elem = document.createElement('div')
     elem.innerText = inner
+    if (copyOnClick) {
+      elem.classList.add('pointer')
+      elem.addEventListener('click', copyToClipboard)
+    }
     return elem
+  }
+  const copyToClipboard = event => {
+    const range = document.createRange()
+    range.selectNode(event.target)
+    window.getSelection().removeAllRanges()
+    window.getSelection().addRange(range)
+    document.execCommand('copy')
+    window.getSelection().removeAllRanges()
   }
 
   window.fetch(url)
@@ -12,7 +24,7 @@
     .then(({ ip, city, region, country_name: countryName, isp, hostname }) => {
       document.title = ip
 
-      el('header').append(div(ip))
+      el('header').append(div(ip, { copyOnClick: true }))
 
       el('section').append(
         div([city, region, countryName].filter(Boolean).join(', ')),
